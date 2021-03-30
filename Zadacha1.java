0a5f3bfbd345a446284e5a5e1c970f40ef0b2f61
heat672k <63456423+heat672k@users.noreply.github.com>
2021-03-30 21:39:32 +0300

    Add files via upload

---
+++ Zadacha1.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class Zadacha1 {
+
+    public static void main(String[] args) {
+        perimetar();
+        lice();
+    }
+    public static void perimetar() {
+        int a = 2, b =  6;
+        int result = 2*a + 2*b;
+        System.out.println("Parametarat e: "+ result );
+    }
+    public static void lice () {
+        int a = 2, b = 6;
+        int result = a*b;
+        System.out.println("Liceto e: "+ result );
+
+    }
+
+
+
+
+}
